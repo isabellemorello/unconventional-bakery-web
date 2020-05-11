531a8fe703491b0991b2e53b7fa7fb29854bf39d
Isabelle Michelle Morello <isabelle.michelle.morello@gmail.com>
2020-05-11 19:44:22 +0200

    Added css file
    Co-authored-by: Puro89 <puro89@live.it>

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 <html>
 
 <head>
+    <meta charset=utf-8>
+    <meta name="keywords" content="HTML">
+    <meta name="description" content="Purinan Unconventional Bakery E-commerce">
+    <meta name="author" content="Morello - Purinan">
     <title>Unconventional Bakery</title>
+    <meta name="viewport" content="width=device-width" />
+    <link href="css/comune.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/desktop.css" rel="stylesheet" type="text/css" media="screen and (min-width: 800px)" />
+    <link rel="stylesheet" href="css/tablet.css" type="text/css" media="screen and (min-width: 481px) and (max-width: 799px)" />
+    <link rel="stylesheet" href="css/smartphone.css" type="text/css" media="screen and (max-width: 480px)" />
+    <link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
 </head>
 
 <body>
